7f2e796af9970268a2332b6a58162ae41a976bb0
steve <stevedanko1@gmail.com>
2014-02-14 15:09:24 -0500

    refacotred

--- dart_board.rb
+++ dart_board.rb
@@ -1,22 +1,16 @@
 def scoreThrows(radiuses)
-  less_than_5_radiuses = []
   score = 0
+  if radiuses.count > 0 && radiuses.all? { |score| score <= 4 && score >= 0 }
+    score += 100
+  end
   if !radiuses.empty?
     radiuses.each do |throw|
       if throw >= 5 && throw <= 10
         score += 5
-        less_than_5_radiuses << "no bonus"
       elsif throw >= 0 && throw < 5 
         score += 10
-      else
-        less_than_5_radiuses << "no bonus"
       end
     end
-    if less_than_5_radiuses.include?("no bonus")
-      score
-    else
-      score += 100
-    end
   end
-    score
+  score
 end
